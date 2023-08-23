093ce3f174b3f67bbb6f036fa3845daa787a9a98
cslunsford <christopher.lunsford@uconn.edu>
2023-08-23 15:26:23 -0400

    added department model and created database in schema file

---
+++ models/department.js
@@ -0,0 +1,23 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Department extends Model {}
+
+Department.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+    },
+}, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: 'Department'
+});
+
+module.exports = Department;
