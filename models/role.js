71597459dbc5fdba820b611351ab0d1f724fe06d
cslunsford <christopher.lunsford@uconn.edu>
2023-08-23 15:49:03 -0400

    added Role model

---
+++ models/role.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const Department = require('./department');
+
+class Role extends Model {}
+
+Role.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+    },
+    salary: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+    },
+    department_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'department',
+            key: 'id',
+        },
+    },
+}, {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    freezeTableName: true,
+    modelName: 'Role'
+});
+
+module.exports = Role;
