9870b210dffeaa44cd3eedfc311ce547e067af45
cslunsford <christopher.lunsford@uconn.edu>
2023-08-23 18:02:02 -0400

    changed formatting for better code structure

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,22 @@
-const inquirerPrompts = require('./prompts');
+const prompts = require('./prompts');
 
-inquirerPrompts.init();
+prompts.initialPrompt()
+    .then((data) => {
+        if (data.choice === 'View All Employees') {
+            queries.viewAllEmployees();
+        } else if (data.choice === 'Add Employee') {
+            queries.addEmployee();
+        } else if (data.choice === 'Update Employee Role') {
+            queries.updateEmployeeRole();
+        } else if (data.choice === 'View All Roles') {
+            queries.viewAllRoles();
+        } else if (data.choice === 'Add Role') {
+            queries.addRole();
+        } else if (data.choice === 'View All Departments') {
+            queries.viewAllDepartments();
+        } else if (data.choice === 'Add Department') {
+            queries.addDepartment();
+        } else {
+            endPrompt();
+        }
+    });
--- scripts/prompts.js
+++ scripts/prompts.js
@@ -1,34 +1,15 @@
 const inquirer = require('inquirer');
 const queries = require('./queries');
 
-function init() {
-    inquirer.prompt([
+function initialPrompt() {
+    return inquirer.prompt([
         {
             type: 'list',
             message: 'What would you like to do?',
             choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
             name: 'choice'
         }
-    ])
-    .then((data) => {
-        if (data.choice === 'View All Employees') {
-            queries.viewAllEmployees();
-        } else if (data.choice === 'Add Employee') {
-            queries.addEmployee();
-        } else if (data.choice === 'Update Employee Role') {
-            queries.updateEmployeeRole();
-        } else if (data.choice === 'View All Roles') {
-            queries.viewAllRoles();
-        } else if (data.choice === 'Add Role') {
-            queries.addRole();
-        } else if (data.choice === 'View All Departments') {
-            queries.viewAllDepartments();
-        } else if (data.choice === 'Add Department') {
-            queries.addDepartment();
-        } else {
-            endPrompt();
-        }
-    });
+    ]);
 };
 
-module.exports = init;
+module.exports = initialPrompt;
